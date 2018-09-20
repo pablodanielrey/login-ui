6c0e4a13e0ee8753f3036c8913b94702dc4e8296
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-20 07:59:05 -0300

    elimine producción y dje solo presi y econo

--- src/login-ui/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
-  loginApiUrl: 'https://api.econo.unlp.edu.ar/login/api/v1.0',
-  logoutUrl:'https://login.econo.unlp.edu.ar/logout/{{id_token}}/{{client_id}}',
-  session: 'SID'  
-};
