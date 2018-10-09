1dcf384a89a28b881a794c7d09faeeac9dddb923
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-10-09 12:24:38 -0300

    impelemente la parte de redireccionar hacia la app cliente

--- src/login-ui/src/app/login/logout/logout.component.ts
+++ src/login-ui/src/app/login/logout/logout.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Location, DOCUMENT } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
 import { LoginService } from '../../login.service';
@@ -11,20 +11,30 @@ import { LoginService } from '../../login.service';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private location: Location, private service: LoginService) {
+  constructor(@Inject(DOCUMENT) private document: any,
+              private route: ActivatedRoute, 
+              private location: Location, 
+              private service: LoginService) {
 
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(p => {
+      let ref = p.get('ref');
       let id_token = p.get('id_token');
       let client_id = p.get('client_id');
       this.service.logout(id_token, client_id).subscribe(
         r => {
-          this.location.back();
+          if (ref) {
+            console.log('redireccionando a ' + ref);
+            this.document.location.href = ref;
+          }
         },
         e => {
-          this.location.back();
+          if (ref) {
+            console.log('redireccionando a ' + ref);
+            this.document.location.href = ref;
+          }
         }
       );
     });
