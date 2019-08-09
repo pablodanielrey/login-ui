c6564db7f60f21f43fa63ec4d0b997699ceea8ad
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-08-09 12:30:20 -0300

    agreguo para que no se redirija a bienvenido. ya con la verificación del consent es suficiente

--- dev/src/login-ui/src/app/modules/consent/verificar-challenge/verificar-challenge.component.ts
+++ dev/src/login-ui/src/app/modules/consent/verificar-challenge/verificar-challenge.component.ts
@@ -32,11 +32,7 @@ export class VerificarChallengeComponent implements OnInit, OnDestroy {
       this.service.get_consent_challenge(this.route).subscribe(r => {
         console.log(r);
         let redirect_url = r['redirect_to'];
-        sessionStorage.setItem('redirect_url', redirect_url);
-        let redirect = btoa(redirect_url);
-        //this.document.location.href = redirect_url;
-        this.router.navigate([`/login/bienvenido/${redirect}`]);
-        //this.router.navigate([`/login/bienvenido/`]);
+        this.document.location.href = redirect_url;
       })
     )    
   }
