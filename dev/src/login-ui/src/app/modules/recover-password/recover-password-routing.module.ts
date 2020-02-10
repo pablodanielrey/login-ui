d23d803fe545f20f444997883ae7315a74f5b6ea
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-02-10 16:40:27 -0300

    elimine la verificacion del correo

--- dev/src/login-ui/src/app/modules/recover-password/recover-password-routing.module.ts
+++ dev/src/login-ui/src/app/modules/recover-password/recover-password-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FinalizeComponent } from './components/finalize/finalize.component';
 import { InputUsernameComponent } from './components/input-username/input-username.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { VerifyCodeComponent } from './components/verify-code/verify-code.component';
 import { InputCredentialsComponent } from './components/input-credentials/input-credentials.component';
 import { ErrorComponent } from './components/error/error.component';
@@ -15,7 +14,6 @@ const routes: Routes = [
             { path: 'start', redirectTo: 'register' },
             { path:'register', component: RegisterDeviceComponent },
             { path:'username', component: InputUsernameComponent },
-            { path:'email', component: VerifyEmailComponent },
             { path:'code', component: VerifyCodeComponent },
             { path:'credentials', component: InputCredentialsComponent },
             { path:'finalize', component: FinalizeComponent },
