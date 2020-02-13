b4365941558a8b8f5b1fa666b8a48bf24a7abf95
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-02-13 20:51:36 -0300

    agrego el entorno de producción

--- dev/src/login-ui/src/environments/environment.prod.ts
+++ dev/src/login-ui/src/environments/environment.prod.ts
@@ -1,3 +1,32 @@
 export const environment = {
-  production: true
+  production: false,
+  loginApiUrl: 'https://login.econo.unlp.edu.ar/login/api/v1.0',
+  recoverApiUrl: 'https://login.econo.unlp.edu.ar/login/api/v1.0',
+  hardwareApiUrl: 'https://login.econo.unlp.edu.ar/login/api/v1.0',
+
+  wardenApiUrl: 'https://api.econo.unlp.edu.ar/warden/api/v1.0',
+  oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
+
+  client_id: 'login-ui',
+  version: '0.1.0',
+
+  loader: {
+    cabecera: 'LOGIN | FCE',
+    version: '0.1.0',
+    tituloSistema: 'LOGIN',
+    subtituloSistema: '',
+    desarrolloSistema: 'DiTeSI | Dirección de Tecnologías y Sistemas Informáticos',
+    logoSistema: '/assets/img/fce/logofce2018.png',
+  },
+  
+  url_to_activate_qr: 'http://login:10005/login/qrcode/activar',
+  
+  auth: {
+    issuer: 'https://oidc.econo.unlp.edu.ar/',
+    redirectUri: 'https://login.econo.unlp.edu.ar/auth/oauth2',
+    clientId: 'login-ui',
+    scope: 'openid profile email',
+    waitForTokenInMsec: 2000,
+    requireHttps: true
+  }
 };
