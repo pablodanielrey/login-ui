2ef9bdc01924af36c5661edcc06bf6962e285252
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-02-19 18:17:34 -0300

    agrego los cambios para poder llamar a la nueva api

--- dev/src/login-ui/src/app/modules/recover-password/components/input-username/input-username.component.ts
+++ dev/src/login-ui/src/app/modules/recover-password/components/input-username/input-username.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HardwareService } from 'src/app/shared/services/hardware.service';
+import { Observable } from 'rxjs';
+import { RecoverPasswordService } from '../../services/recover-password.service';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-input-username',
@@ -11,23 +15,40 @@ export class InputUsernameComponent implements OnInit {
 
   accediendo = false;
   form: FormGroup;
+  device_hash$: Observable<string>
 
-  constructor(private fb: FormBuilder, private router:Router) { 
+  private subs = [];
+
+  ngOnDestroy(): void {
+    this.subs.forEach(s => s.unsubscribe());
+  }
+
+
+  constructor(private fb: FormBuilder, 
+              private router:Router,
+              private hardware: HardwareService,
+              private service: RecoverPasswordService) { 
 
     this.form = fb.group({
-      usuario: ['', [Validators.required, Validators.minLength(5), Validators.pattern("[a-zA-Z0-9]+")]]
+      user: ['', [Validators.required, Validators.minLength(5), Validators.pattern("[a-zA-Z0-9]+")]]
     })    
 
   }
 
   ngOnInit() {
+    this.device_hash$ = this.hardware.get_device_id();
   }
 
-  verificar() {
+  verify() {
     if (!this.form.valid) {
       return;
     }
-    this.router.navigate(['/recover/code']);
+    this.device_hash$.pipe(
+      map(hash => this.service.recover_for(this.form.value['user'], hash))
+    ).subscribe(
+      r => this.router.navigate(['/recover/code']),
+      e => console.log('error')
+    );
   }
 
 }
--- dev/src/login-ui/src/app/modules/recover-password/services/recover-password.service.ts
+++ dev/src/login-ui/src/app/modules/recover-password/services/recover-password.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../../environments/environment';
+import { HardwareService } from 'src/app/shared/services/hardware.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+export interface Response {
+  status:number,
+  response:any
+}
 
 
 @Injectable({
@@ -12,10 +19,18 @@ export class RecoverPasswordService {
 
   url: string = null;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private hardare: HardwareService) { 
     this.url = environment.recoverApiUrl;
   }
 
-
+  recover_for(user:string, device:string) : Observable<string> {
+    let url = `${this.url}/recover/${user}`;
+    let data = {
+      device: device
+    }
+    return this.http.post<Response>(url, data).pipe(
+      map(r => r.response)
+    );    
+  }
 
 }
