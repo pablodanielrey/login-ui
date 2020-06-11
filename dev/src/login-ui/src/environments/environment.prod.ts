84ddfe54648bae8ac61d4ce92bd491e3b1bbe366
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-06-10 22:04:18 -0300

    agrego una nueva version que cambia los dominios para poder pasarlo a producción dentro de kubernetes

--- dev/src/login-ui/src/environments/environment.prod.ts
+++ dev/src/login-ui/src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: false,
-  loginApiUrl: 'https://login.econo.unlp.edu.ar/login/api/v1.0',
-  emailApiUrl: 'https://login.econo.unlp.edu.ar/email/api/v1.0',
+  loginApiUrl: 'https://hydra.login.econo.unlp.edu.ar/login/api/v1.0',
+  emailApiUrl: 'https://hydra.login.econo.unlp.edu.ar/email/api/v1.0',
   recoverApiUrl: 'https://login.econo.unlp.edu.ar/recover/api/v1.0',
-  hardwareApiUrl: 'https://login.econo.unlp.edu.ar/login/api/v1.0',
+  hardwareApiUrl: 'https://hydra.login.econo.unlp.edu.ar/login/api/v1.0',
 
-  wardenApiUrl: 'https://api.econo.unlp.edu.ar/warden/api/v1.0',
+  wardenApiUrl: 'https://warden.econo.unlp.edu.ar/warden/api/v1.0',
   oidp_issuer: 'https://oidc.econo.unlp.edu.ar/',
 
   client_id: 'login-ui',
-  version: '0.3.0',
+  version: '0.4.0',
   
   url_to_activate_qr: 'http://login:10005/login/qrcode/activar',
   
