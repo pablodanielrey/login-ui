852fa3465f1f85e7a23ff949e3bc89e7333976fa
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-06-17 18:09:44 -0300

    corregí la redirección

--- dev/utils/crear_login-ui.sh
+++ dev/utils/crear_login-ui.sh
@@ -4,6 +4,6 @@ docker run --rm -ti oryd/hydra:v1.5 clients create --skip-tls-verify --fake-tls-
     --secret pppppppppp \
     --grant-types implicit \
     --response-types id_token,token,"id_token token" \
-    --callbacks http://http://localhost:10005/auth/oauth2 \
+    --callbacks http://localhost:10005/auth/oauth2 \
     --scope openid,profile,offline,* \
     --client-uri https://localhost:10005
