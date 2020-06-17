021bb946f2a3b91fcf4ab49a7c62a709a526a849
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-06-17 17:32:17 -0300

    tenia que ser mas largo el secret

--- dev/utils/crear_login-ui.sh
+++ dev/utils/crear_login-ui.sh
@@ -1,7 +1,7 @@
 docker run --rm -ti oryd/hydra:v1.5 clients delete login-ui --fake-tls-termination --endpoint http://192.168.10.3:4445
 docker run --rm -ti oryd/hydra:v1.5 clients create --skip-tls-verify --fake-tls-termination --endpoint http://192.168.10.3:4445 \
     --id login-ui \
-    --secret ppp \
+    --secret pppppppppp \
     --grant-types implicit \
     --response-types id_token,token,"id_token token" \
     --callbacks http://http://localhost:10005/auth/oauth2 \
